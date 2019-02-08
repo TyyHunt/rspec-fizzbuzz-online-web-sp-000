aba232c6d7c65c21287bfce62b24c4170bd9ebdb
Tyler Hunt <tyhunt04@gmail.com>
2019-02-08 18:25:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-1.upto(1000) do |i|
+
+1.upto 1000 do |i|
+  puts i 
   if i % 3 == 0 && i % 5 == 0
     puts "Fizzbuzz"
   elsif i % 3 == 0
     puts "Fizz"
   elsif i % 5 == 0
     puts "Buzz"
-  else returns nil
   end
